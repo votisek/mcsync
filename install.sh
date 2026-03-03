c92fa5e3742378954c639d6525a986ecb36a9ff0
Vojta Vávra <votisek@proton.me>
2026-03-03 11:11:40 +0100

    frfr install works

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 clear
 
 echo "Installing mcsync..."
-curl -O ~/.config/mcsr/scripts/mcsync raw.githubusercontent.com/votisek/mcsync/main/mcsync
+curl "https://raw.githubusercontent.com/votisek/mcsync/main/mcsync" --output "lol.sh"
 chmod +x ~/.config/mcsr/scripts/mcsync
 
 echo "mcsync installed, to start using it, follow the README in the github repository here: https://github.com/votisek/mcsync"
