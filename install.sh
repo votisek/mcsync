cc0821adcbb405b9d2e08997c5bc697b4bf5bb13
Vojta Vávra <votisek@proton.me>
2026-03-03 11:36:46 +0100

    working now, is usable

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 clear
 
 echo "Installing mcsync..."
-curl "https://raw.githubusercontent.com/votisek/mcsync/main/mcsync" --output "lol.sh"
+mkdir -p ~/.config/mcsr/
+curl "https://raw.githubusercontent.com/votisek/mcsync/main/mcsync" --output "~/.config/mcsr/scripts/mcsync"
 chmod +x ~/.config/mcsr/scripts/mcsync
 
 echo "mcsync installed, to start using it, follow the README in the github repository here: https://github.com/votisek/mcsync"
